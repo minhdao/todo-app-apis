3f685834288342a745356c3278f2cd8ffff107c0
minhdao <minhdao6@gmail.com>
2018-01-30 16:53:23 +0700

    config routes to get all todos added in db

--- server/server.js
+++ server/server.js
@@ -27,6 +27,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);	
+    });
+});
+
 // start server
 app.listen(3000, () => {
     console.log('Server started on port 3000...');
