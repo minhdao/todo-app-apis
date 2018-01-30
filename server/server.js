599c06292c6682f9724ff8398aff07e774361cf5
minhdao <minhdao6@gmail.com>
2018-01-30 12:43:45 +0700

    import Todo and User model

--- server/server.js
+++ server/server.js
@@ -1,1 +1,6 @@
+// load in/import the mongoose
 var {mongoose} = require('./db/mongoose.js');
+
+// load in models
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
