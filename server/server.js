6a5c156ac51bf4e806feb5c8765007cc0cf45d72
minhdao <minhdao6@gmail.com>
2018-01-30 13:45:32 +0700

    save user and send response back to user

--- server/server.js
+++ server/server.js
@@ -6,3 +6,28 @@ var {mongoose} = require('./db/mongoose.js');
 // load in models
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+
+var app = express();
+
+// middleware
+app.use(bodyParser.json()); // take json -> object
+
+// routes config
+app.post('/todos', (req, res) => {
+    // create todo model object
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    // save todo into database
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.send(err);
+    });
+});
+
+// start server
+app.listen(3000, () => {
+    console.log('Server started on port 3000...');
+});
