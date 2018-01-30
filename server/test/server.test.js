fa5e803023bb99c27eb1cb1c8552a304cf66026b
minhdao <minhdao6@gmail.com>
2018-01-30 16:05:10 +0700

    create test for post /todos

---
+++ server/test/server.test.js
@@ -0,0 +1,31 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server.js');
+const {Todo} = require('./../models/todo.js');
+
+describe('POST /todos', () => {
+    it('should create new todo', (done) => {
+        var text = 'todo test';
+        request(app)
+            .post('/todos')
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) => {
+                if (err){
+                    return done(err);
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((err) => {
+                	done(err);
+                });
+            });
+
+    });
+});
