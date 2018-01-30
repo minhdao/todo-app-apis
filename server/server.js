d8d69ea6f921259de3813ab2654af1b58dbfe8f0
minhdao <minhdao6@gmail.com>
2018-01-30 12:48:14 +0700

    import express and body-parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 // load in/import the mongoose
 var {mongoose} = require('./db/mongoose.js');
-
 // load in models
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
