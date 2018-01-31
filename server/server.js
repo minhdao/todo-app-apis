a664f72c08c587c8eda0bfa892b1631db1a28cd8
minhdao <minhdao6@gmail.com>
2018-01-31 13:05:13 +0700

    add get new route GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -36,6 +36,21 @@ app.get('/todos', (req, res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((err) => {
+        res.status(400).send();    
+    });
+});
+
 // start server
 app.listen(3000, () => {
     console.log('Server started on port 3000...');
