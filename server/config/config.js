f75b491b4992f6a75f84ffade7e8487f5eb60c67
minhdao <minhdao6@gmail.com>
2018-03-12 14:56:57 +0700

    clean up code

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,7 @@ console.log(`******** env ${env}`);
 
 if (env === 'development' || env === 'test'){
     var config = require('./config.json');
-    console.log(config);
     var envConfig = config[env];
-    console.log(envConfig);
-    console.log(Object.keys(envConfig));
     Object.keys(envConfig).forEach((key) => {
         process.env[key] = envConfig[key];
     });
